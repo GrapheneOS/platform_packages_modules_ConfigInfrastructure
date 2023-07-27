82ddc6066bea89127ce99686fb5876cf767471db
Ted Bauer <tedbauer@google.com>
2023-07-27 17:08:26 +0000

    Remove need for READ_DEVICE_CONFIG to read flags.
    Test: m
    Bug: 288298466
    Change-Id: Ie0cb5cfa14dbaf84714157e50f949793f40585f6

--- framework/java/android/provider/DeviceConfig.java
+++ framework/java/android/provider/DeviceConfig.java
@@ -1085,7 +1085,6 @@ public final class DeviceConfig {
      * @hide
      */
     @SystemApi
-    @RequiresPermission(READ_DEVICE_CONFIG)
     @Nullable
     public static String getProperty(@NonNull String namespace, @NonNull String name) {
         // Fetch all properties for the namespace at once and cache them in the local process, so we
@@ -1150,7 +1149,6 @@ public final class DeviceConfig {
      * @hide
      */
     @SystemApi
-    @RequiresPermission(READ_DEVICE_CONFIG)
     public static boolean getBoolean(@NonNull String namespace, @NonNull String name,
             boolean defaultValue) {
         String value = getProperty(namespace, name);
